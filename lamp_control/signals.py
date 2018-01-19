b738e982ea4b7017ec71c25b96871d79f3cfeaf8
steven <steven.pilatschek@gmail.com>
2018-01-19 15:12:42 +0100

    First attempt of toggling the lamps.

--- lamp_control/signals.py
+++ lamp_control/signals.py
@@ -1,8 +1,18 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from gpiozero import LED
 
 from lamp_control.models import Lamp, Log
 
+
 @receiver(post_save, sender=Lamp)
 def create_lamp_log(sender, **kwargs):
-    Log.objects.create(lamp=kwargs["instance"])
+
+    lamp = kwargs["instance"]
+    led = LED(lamp.gpiopin)
+    if lamp.is_on:
+        led.off()
+    else:
+        led.on()
+
+    Log.objects.create(lamp=lamp)
